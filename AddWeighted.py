c53d995e47f7666697f380377abef648cca797f2
EightSky <kw308@naver.com>
2023-03-17 23:36:59 +0900

    AddWeighted.py

--- AddWeighted.py
+++ AddWeighted.py
@@ -11,7 +11,7 @@ addWeighted = cv2.addWeighted(img1, alpha, img2, (1-alpha), 0)
 #img2의 가중치 = (1-alpha) 
 
 #addWeighted(A, a, B, b, C)
-#A,B 그림, a,b 해당 그림 가중치 [A의 가중치 a, B의 가중치b]
+#A,B 그림, a,b 해당 영상 가중치 [A의 가중치 a, B의 가중치b]
 #C 결과 이미지에 추가적으로 더할 값
 
 cv2.imshow('cv2.addWeighted', addWeighted)
